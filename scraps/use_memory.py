7864db6c9a9d4c4d70a6b8eb79b0c55694dd034c
Jonathan Moore <jmoore@localhost.home>
2015-06-29 22:50:10 +0100

    handle program start & finish

--- scraps/use_memory.py
+++ scraps/use_memory.py
@@ -4,13 +4,19 @@ import sys
 import time
 
 if len(sys.argv) != 2:
-    print "usage: use_memory <number-of-gigabytes>"
-    sys.exit()
+    sys.exit("usage: use_memory <number-of-gigabytes>")
 
 count = int(sys.argv[1])
 
-gigabyte = (0,) * (1024*1024 * 1024 / 8)
+gigabyte = (0,) * (1024 * 1024 / 8 * 1000)
 data = gigabyte * count
-
-while True:
-    time.sleep(1)
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    e = "goodbye"
+except Exception as e:
+    pass # handle in finally clause
+finally:
+    del data
+    sys.exit(str(e))
